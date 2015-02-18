894821f58250c66ff46493fbf9eec457afed451f
Rob Rix <rob.rix@github.com>
2015-02-17 23:40:25 -0500

    Implement disjunction over Optional.

--- Either/Disjunction.swift
+++ Either/Disjunction.swift
@@ -1,5 +1,12 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
+// MARK: - Optional disjunction
+
+func ||| <T, U> (left: T?, right: U?) -> Either<T, U>? {
+	return left.map(Either.left) ?? right.map(Either.right)
+}
+
+
 // MARK: - Operators
 
 infix operator ||| {
