8ceeeeb46ed181672c1e8868b40fd50f779bccce
Rob Rix <rob.rix@github.com>
2015-02-17 23:43:15 -0500

    Short-circuit disjunction.

--- Either/Disjunction.swift
+++ Either/Disjunction.swift
@@ -5,8 +5,8 @@
 /// Returns an `Either` representing the `left` operand if non-`nil`, or else the `right` if non-`nil`, or else `nil`.
 ///
 /// Dual to `&&&`, defined in Prelude.
-func ||| <T, U> (left: T?, right: U?) -> Either<T, U>? {
-	return left.map(Either.left) ?? right.map(Either.right)
+func ||| <T, U> (left: T?, @autoclosure right: () -> U?) -> Either<T, U>? {
+	return left.map(Either.left) ?? right().map(Either.right)
 }
 
 
