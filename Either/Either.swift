f945346048fc2b7b3ead9f94f9aca7a43e53a691
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-09-17 13:05:05 +0900

    correct along the swift3 notation

--- Either/Either.swift
+++ Either/Either.swift
@@ -15,14 +15,14 @@ public enum Either<T, U>: EitherType, CustomDebugStringConvertible, CustomString
 	/// Constructs a `Left`.
 	///
 	/// Suitable for partial application.
-	public static func left(value: T) -> Either {
+	public static func left(_ value: T) -> Either {
 		return Left(value)
 	}
 
 	/// Constructs a `Right`.
 	///
 	/// Suitable for partial application.
-	public static func right(value: U) -> Either {
+	public static func right(_ value: U) -> Either {
 		return Right(value)
 	}
 
@@ -30,7 +30,7 @@ public enum Either<T, U>: EitherType, CustomDebugStringConvertible, CustomString
 	// MARK: API
 
 	/// Returns the result of applying `f` to the value of `Left`, or `g` to the value of `Right`.
-	public func either<Result>(@noescape ifLeft ifLeft: T throws -> Result, @noescape ifRight: U throws -> Result) rethrows -> Result {
+	public func either<Result>(ifLeft: (T) throws -> Result, ifRight: (U) throws -> Result) rethrows -> Result {
 		switch self {
 		case let .Left(x):
 			return try ifLeft(x)
@@ -40,24 +40,24 @@ public enum Either<T, U>: EitherType, CustomDebugStringConvertible, CustomString
 	}
 
 	/// Maps `Right` values with `transform`, and re-wraps `Left` values.
-	public func map<V>(@noescape transform: U -> V) -> Either<T, V> {
+	public func map<V>(_ transform: (U) -> V) -> Either<T, V> {
 		return flatMap { .right(transform($0)) }
 	}
 
 	/// Returns the result of applying `transform` to `Right` values, or re-wrapping `Left` values.
-	public func flatMap<V>(@noescape transform: U -> Either<T, V>) -> Either<T, V> {
+	public func flatMap<V>(_ transform: (U) -> Either<T, V>) -> Either<T, V> {
 		return either(
 			ifLeft: Either<T, V>.left,
 			ifRight: transform)
 	}
 
 	/// Maps `Left` values with `transform`, and re-wraps `Right` values.
-	public func mapLeft<V>(@noescape transform: T -> V) -> Either<V, U> {
+	public func mapLeft<V>(_ transform: (T) -> V) -> Either<V, U> {
 		return flatMapLeft { .left(transform($0)) }
 	}
 
 	/// Returns the result of applying `transform` to `Left` values, or re-wrapping `Right` values.
-	public func flatMapLeft<V>(@noescape transform: T -> Either<V, U>) -> Either<V, U> {
+	public func flatMapLeft<V>(_ transform: (T) -> Either<V, U>) -> Either<V, U> {
 		return either(
 			ifLeft: transform,
 			ifRight: Either<V, U>.right)
@@ -80,7 +80,7 @@ public enum Either<T, U>: EitherType, CustomDebugStringConvertible, CustomString
 
 
 	/// Given equality functions for `T` and `U`, returns an equality function for `Either<T, U>`.
-	public static func equals(left left: (T, T) -> Bool, right: (U, U) -> Bool) -> (Either<T, U>, Either<T, U>) -> Bool {
+	public static func equals(left: @escaping (T, T) -> Bool, right: @escaping (U, U) -> Bool) -> (Either<T, U>, Either<T, U>) -> Bool {
 		return { a, b in
 				(a.left &&& b.left).map(left)
 			??	(a.right &&& b.right).map(right)
@@ -113,7 +113,7 @@ public enum Either<T, U>: EitherType, CustomDebugStringConvertible, CustomString
 /// If `left` is `Either.Right`, extracts its value and passes it to `transform`, returning the result; otherwise transforms `either` into the return type.
 ///
 /// This is the bind or flat map operator, and is useful for chaining computations taking some parameter and returning an `Either`.
-public func >>- <T, U, V> (either: Either<T, U>, @noescape transform: U -> Either<T, V>) -> Either<T, V> {
+public func >>- <T, U, V> (either: Either<T, U>, transform: (U) -> Either<T, V>) -> Either<T, V> {
 	return either.flatMap(transform)
 }
 
