8a3bb9baa2efde3e63b371ee7eb43bb1886f5a92
Shintaro Morikawa <sntr92@gmail.com>
2017-03-14 23:36:57 +0900

    Make lefts/rights computeed properties

--- Either/Extension.swift
+++ Either/Extension.swift
@@ -2,12 +2,12 @@
 
 extension Sequence where Iterator.Element: EitherProtocol {
 	/// Select only `Right` instances.
-	public func rights() -> [Iterator.Element.Right] {
-		return self.flatMap{ $0.right }
+	public var rights: [Iterator.Element.Right] {
+		return flatMap{ $0.right }
 	}
 	
 	/// Select only `Left` instances.
-	public func lefts() -> [Iterator.Element.Left] {
-		return self.flatMap{ $0.left }
+	public var lefts: [Iterator.Element.Left] {
+		return flatMap{ $0.left }
 	}
 }
--- EitherTests/ExtensionTests.swift
+++ EitherTests/ExtensionTests.swift
@@ -9,11 +9,11 @@ final class ExtensionTests: XCTestCase {
 	
 	func testRightsSelectOnlyRight() {
 		let list: [Either<Int, Int>] = [.right(0), .left(1), .left(2), .right(3)]
-		XCTAssertEqual(list.rights(), [0, 3])
+		XCTAssertEqual(list.rights, [0, 3])
 	}
 	
 	func testLeftsSelectOnlyLeft() {
 		let list: [Either<Int, Int>] = [.right(0), .left(1), .left(2), .right(3)]
-		XCTAssertEqual(list.lefts(), [1, 2])
+		XCTAssertEqual(list.lefts, [1, 2])
 	}
 }
