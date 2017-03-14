3ced0ec59f636256219e37ded9befdb3bf2203ea
Shintaro Morikawa <sntr92@gmail.com>
2017-03-14 23:22:48 +0900

    Avoid using implicitly unwrapped optional type

--- Either/Extension.swift
+++ Either/Extension.swift
@@ -3,11 +3,11 @@
 extension Sequence where Iterator.Element: EitherProtocol {
 	/// Select only `Right` instances.
 	public func rights() -> [Iterator.Element.Right] {
-		return self.filter{ $0.isRight }.map{ $0.right! }
+		return self.flatMap{ $0.right }
 	}
 	
 	/// Select only `Left` instances.
 	public func lefts() -> [Iterator.Element.Left] {
-		return self.filter{ $0.isLeft }.map{ $0.left! }
+		return self.flatMap{ $0.left }
 	}
 }
